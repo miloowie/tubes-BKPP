f95e3f5b18dfd1d0943cda80e7a4d214526dc1f6
miloowie <reivenvhista@gmail.com>
2025-12-12 13:43:36 +0700

    update

---
+++ kalkul_BKPP.go
@@ -0,0 +1,96 @@
+package main
+
+import "fmt"
+
+func main() {
+	var bb, tb float64
+	var gender string
+	var umur int
+	var usia bool
+
+	fmt.Println("Kalkulator BMI")
+	for {
+		for {
+			fmt.Print("Jenis Kelamin (L/P): ")
+			fmt.Scan(&gender)
+
+			if gender == "L" || gender == "P" {
+				break
+			}
+
+			fmt.Println("Input tidak valid!")
+		}
+
+		for {
+			fmt.Print("Apakah usia anda lebih dari atau sama dengan 18 tahun? (true/false): ")
+			fmt.Scan(&usia)
+			fmt.Print("Berapa umur anda: ")
+			fmt.Scan(&umur)
+
+			if umur > 0 && umur >= 18 && usia {
+				break
+			} else if umur > 0 && umur < 18 && !usia {
+				fmt.Println("Perhitungan BMI mungkin tidak akurat karena umur di bawah 18 tahun.")
+				fmt.Print("Lanjutkan perhitungan BMI? (ya/tidak): ")
+				var lanjut string
+				fmt.Scan(&lanjut)
+
+				if lanjut == "ya" {
+					break
+				} else if lanjut == "tidak" {
+					fmt.Println("Program selesai, terima kasih.")
+					return
+				} else {
+					fmt.Println("Input tidak valid! Mohon masukkan 'ya' atau 'tidak'.")
+				}
+
+			} else {
+				fmt.Println("Input tidak valid! Mohon masukkan variabel yang valid!.")
+			}
+		}
+
+		for {
+			fmt.Print("Berat Badan (kg): ")
+			fmt.Scan(&bb)
+
+			fmt.Print("Tinggi Badan (cm): ")
+			fmt.Scan(&tb)
+
+			if bb > 0 && tb > 0 {
+				break
+			}
+
+			fmt.Println("Input tidak valid, mohon masukkan angka yang valid!")
+		}
+
+		bmi := bb / ((tb / 100) * (tb / 100))
+		fmt.Printf("BMI Anda: %.2f\n", bmi)
+
+		if bmi < 18.5 {
+			fmt.Println("Kategori: Kurus")
+		} else if bmi < 25 {
+			fmt.Println("Kategori: Normal")
+		} else if bmi < 30 {
+			fmt.Println("Kategori: Gemuk")
+		} else {
+			fmt.Println("Kategori: Obesitas")
+		}
+
+		var ulang string
+		for {
+			fmt.Print("Hitung lagi? (y/n): ")
+			fmt.Scan(&ulang)
+			if ulang == "y" {
+				main()
+				return
+			} else if ulang == "n" {
+				fmt.Println("Program selesai.")
+				return
+			}
+
+			fmt.Println("Input tidak valid! Masukkan hanya y atau n.")
+		}
+
+	}
+	fmt.Println()
+}
